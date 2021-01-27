b4657519c27c13fdfc96ce1b5f665c9b781b2236
hase6271 <60119947+hase6271@users.noreply.github.com>
2021-01-27 14:52:16 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -12,7 +12,7 @@ var urlsToCache = [
     '/js/mixin_closing.js',
     '/js/jquery.fancybox.min.js',
     '/js/jquery.fancybox.min.css',
-    'icon.png',
+    '/images/icon.png',
     '/images/bk0.png'
 ];
 
